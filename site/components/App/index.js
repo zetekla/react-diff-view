db8cb8f860acc3687738fa088807a2b1d413fa7e
Pristine Tran <phuctran@paypal.com>
2018-10-24 16:52:42 -0700

    figured the formatLines,unidiff etc. to pipeline oldSource and newSource

--- site/components/App/index.js
+++ site/components/App/index.js
@@ -1,46 +1,56 @@
 import {PureComponent, Fragment} from 'react';
 import uniqueId from 'lodash/uniqueId';
 import sha from 'sha1';
-import {createSelector} from 'reselect';
 import {parseDiff} from 'react-diff-view';
+import {formatLines, diffLines} from 'unidiff';
 import {establishConfiguration} from '../../regions';
 import {DiffView, Configuration} from '../../containers';
 import InputArea from '../InputArea';
 import styles from './index.css';
 
 const fakeIndex = () => sha(uniqueId()).slice(0, 9);
+/*
+const data = {
+    'diff': formatLines(diffLines('The Way that can be told of is not the eternal Way', 'The Way that can be told of is NOT the eternal Way'), {context: 3}),
+    'source': 'The Way that can be told of is not the eternal Way'
+};
+*/
 
 class App extends PureComponent {
 
     state = {
-        diff: null,
-        source: null
+      'diff': formatLines(diffLines('The Way that can be told of is not the eternal Way', 'The Way that can be told of is NOT the eternal Way'), {context: 3}),
+      'source': 'The Way that can be told of is not the eternal Way'
     };
 
-    parse = createSelector(
-        state => state.diff,
-        diff => {
-            if (!diff) {
-                return null;
-            }
-
-            const segments = [
-                'diff --git a/a b/b',
-                `index ${fakeIndex()}..${fakeIndex()} 100644`,
-                diff
-            ];
-
-            const files = parseDiff(segments.join('\n'), {nearbySequences: 'zip'});
-
-            return files[0];
+    parse = diff => {
+        if (!diff) {
+            return null;
         }
-    );
 
-    receiveInput = data => this.setState(data);
+        const segments = [
+            'diff --git a/a b/b',
+            `index ${fakeIndex()}..${fakeIndex()} 100644`,
+            diff
+        ];
+
+        const files = parseDiff(segments.join('\n'), {nearbySequences: 'zip'});
+
+        return files[0];
+    };
+
+    receiveInput = data => {
+        console.log('receiveInput data', JSON.stringify(data, null, 4));
+        this.setState(data);
+    }
 
     render() {
         const {diff, source} = this.state;
-        const file = this.parse(this.state);
+        console.log('DIFF', diff);
+        const file = this.parse(diff);
+        // const file = this.parse(data.diff);
+
+        // const file = false;
 
         console.warn('FILE', JSON.stringify(file));
 
